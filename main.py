7dafbdf7a042b2a80ffc7329b60167851cbc7a0f
Hoangttrieu <trieuhhoang9@gmail.com>
2018-07-30 17:35:20 +0200

    update

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ from readfile import readfile
 from jsonFiles import *
 from callWireshark import *
 def __main__(pathfile):
-    groupname,coordinates,filename,pci=readfile(pathfile)
+    groupname,coordinates,filename,pci,earfcn=readfile(pathfile)
     pathjson=callWireshark(groupname,filename)
-    processingJson(pathjson,coordinates,filename,pci)
+    processingJson(pathjson,coordinates,filename,pci,earfcn)
 
-__main__(r"C:\Users\trieuhoang\Desktop\document\Documentations\RennesBrestVoiture\zk_0000013797_20180404120733_test.txt")
+__main__(r"C:\Users\Trieu Hoang\Desktop\ZKCellTest\RennesBrestVoiture\zk_0000013797_20180404120733_test.txt")
